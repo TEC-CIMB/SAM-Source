43b6986ebc33b7d94f83ae95c3210627d27f6b30
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2022-10-28 19:23:51 -0500

    Synced

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <ArduinoJson.h>
+#include <ESPmDNS.h>
+#include <esp_wifi.h>
 
-#define ARRSIZE 10
+#define ARRSIZE 60
 #define VOLT 2
+#define DEVICE 1
+#define BUTTON 4
 
 const char *ssid = "SAM";
 const char *password = "123456789";
@@ -13,8 +17,9 @@ WebServer server(80);
 StaticJsonDocument<3000> jsonDocument;
 char buffer[3000];
 
-int volt[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
+int volt[ARRSIZE];
+int64_t tim = 0;
+int64_t but = 0;
 TaskHandle_t Task1;
 TaskHandle_t Task2;
 
@@ -29,12 +34,12 @@ void push(int val, int *arreglo)
 
 void inicializarPines()
 {
-  pinMode(VOLT, INPUT_PULLUP);
+  pinMode(BUTTON, INPUT_PULLUP);
 }
 
 void aniadirArreglo(char *nombre, int *arreglo)
 {
-  StaticJsonDocument<200> aux;
+  StaticJsonDocument<2000> aux;
   JsonArray array = aux.to<JsonArray>();
   for (int i = 0; i < ARRSIZE; i++)
   {
@@ -45,20 +50,24 @@ void aniadirArreglo(char *nombre, int *arreglo)
 
 void leerVoltaje(int pin, int *arreglo)
 {
-  push(digitalRead(pin), arreglo);
+  push(analogRead(pin), arreglo);
 }
 
 void getData()
 {
-  Serial.println("Get data");
+  // Serial.println("Get data");
   jsonDocument.clear();
+  jsonDocument["device"] = DEVICE;
+  jsonDocument["time"] = but;
   aniadirArreglo((char *)"volt", volt);
   serializeJson(jsonDocument, buffer);
   server.send(200, "application/json", buffer);
 }
 
-void post()
+void sync()
 {
+  // esp wifi get tsf time
+  tim = esp_wifi_get_tsf_time(WIFI_IF_STA) / 1000 - millis();
   String body = server.arg("plain");
   deserializeJson(jsonDocument, body);
   // Execute after post
@@ -67,6 +76,7 @@ void post()
 
 void wifi(void *pvParameters)
 {
+
   WiFi.begin(ssid, password);
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
@@ -75,12 +85,21 @@ void wifi(void *pvParameters)
     Serial.print(".");
   }
   Serial.println(".");
+  String name = "sam" + String(DEVICE);
+  if (!MDNS.begin(name.c_str()))
+  {
+    Serial.println("Error setting up MDNS responder!");
+    while (1)
+    {
+      delay(1000);
+    }
+  }
   IPAddress IP = WiFi.localIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
   server.enableCORS();
   server.on("/data", getData);
-  server.on("/servo", HTTP_POST, post);
+  server.on("/sync", HTTP_POST, sync);
   server.begin();
   for (;;)
   {
@@ -95,6 +114,12 @@ void hardware(void *pvParameters)
   for (;;)
   {
     leerVoltaje(VOLT, volt);
+    if (digitalRead(BUTTON) == LOW)
+    {
+      but = tim + millis();
+      Serial.print("Button pressed at: ");
+      Serial.println(but);
+    }
     vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }
